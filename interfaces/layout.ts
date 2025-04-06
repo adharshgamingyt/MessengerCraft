a96b9000b6fefd05f1d3ccb0067b7a1501c9e518
adharsh <lordgamingyt648@gmail.com>
2025-04-06 09:24:13 +0530

    change(type): added interface to define type's
    - added layout.ts to type layouts

---
+++ interfaces/layout.ts
@@ -0,0 +1,3 @@
+export interface layout {
+  children: React.ReactNode;
+}
--- src/app/(root)/(home)/layout.tsx
+++ src/app/(root)/(home)/layout.tsx
@@ -1,10 +1,7 @@
 import { Navbar } from "@/components/my ui/Home/navbar";
+import type { layout } from "@/interfaces/layout";
 
-const layout = ({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) => {
+const layout = ({ children }: layout) => {
   return (
     <div className="h-full w-full bg-black">
       <Navbar />
