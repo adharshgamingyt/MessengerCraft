34007c3b8fbc0f13423387c6c54c15af5feca70a
adharsh <lordgamingyt648@gmail.com>
2025-04-23 20:17:19 +0530

    fix: correct formatting in authRoute and add apiAuthPrefix and DEFAULT_LOGIN_REDIRECT

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,7 @@
 export const publicRoutes = ["/", "/about"];
 
-export const authRoute = ["/auth/login","/auth/register",]
+export const authRoute = ["/auth/login", "/auth/register"];
 
+export const apiAuthPrefix = "/api/auth";
+
+export const DEFAULT_LOGIN_REDIRECT = "/dashboard";
