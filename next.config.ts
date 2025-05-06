c05247d9b45f1ff96e5df13bd3380cdb9d1c65cf
adharsh <lordgamingyt648@gmail.com>
2025-05-06 10:23:40 +0530

    chore: moving out of nextjs canary cause of it's error's and lack of docs

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    ppr: "incremental",
-    dynamicIO: true,
-  },
+  experimental: {},
 };
 
 export default nextConfig;
