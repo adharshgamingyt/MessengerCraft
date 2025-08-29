3575737a39cd87b8766c2b82dbc89cb2af34e9c4
adharsh <lordgamingyt648@gmail.com>
2025-08-29 21:24:43 +0530

    feat: initialize api and docs apps with layout, page, and configuration files

---
+++ apps/api/app/layout.tsx
@@ -0,0 +1,32 @@
+import { Ubuntu, Poppins } from "next/font/google";
+
+import "@workspace/ui/globals.css";
+import { Providers } from "@/components/providers";
+
+const fontPoppins = Poppins({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+  variable: "--font-poppins",
+});
+
+const fontUbuntu = Ubuntu({
+  weight: ["300", "400", "500", "700"],
+  subsets: ["latin"],
+  variable: "--font-ubuntu",
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={`${fontPoppins.variable} ${fontUbuntu.variable} font-ubuntu antialiased `}
+      >
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  );
+}
